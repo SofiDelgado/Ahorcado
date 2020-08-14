0f41af30ea0165fd4a0b1a681a0ef1c8a89a6999
Guti9999 <69437959+Guti9999@users.noreply.github.com>
2020-08-14 06:33:04 -0300

    Correcciones y Codigo de ingreso de palabras
    se corrigieron condicionales en la libreria y se agrego la funcion para ingresar la palabra y un test en el codigo principal

--- Ahorcado/Ahorcado.c
+++ Ahorcado/Ahorcado.c
@@ -1,7 +1,39 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <Ctype.h>
+
+
+void IngresarPalabra(char X[20]);
+
+int main(){
+  //char B[15]={'_','_','_','_','_','_','_','_','_','_','_','_','_','_','_',};
+  char A[20];
+  IngresarPalabra(A);
+  for (int i = 0; i < 20; i++) {
+    if (A[i]=='.') {
+      break;
+    }else{
+      printf("%c",A[i] );
+    }
+  }
 
 
 
-main(){
 
+
+
+}
+
+void IngresarPalabra(char X[20]){
+
+    printf("Jugador A, ingresa una palabra letra por letra para empezar el juego!\n");
+
+    for (int i = 0; i < 20; i++) {
+      scanf("%c",&X[i]);
+      X[i]=toupper(X[i]);
+      if (X[i]=='.') {
+        break;
+      }
+
+    }
 }
